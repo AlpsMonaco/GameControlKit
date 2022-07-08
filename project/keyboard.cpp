aa3e8bf14332f968e748c6bc4a936a143be0a2b3
kezhengjie <kzj961014@qq.com>
2022-07-08 11:46:02 +0800

    fix keyboard listener as singleton

--- project/keyboard.cpp
+++ project/keyboard.cpp
@@ -2,30 +2,70 @@
 
 using namespace controlkit;
 
-KeyboardListener::Handler KeyboardListener::handler_        = nullptr;
-KeyboardListener::HandlerMap KeyboardListener::handler_map_ = KeyboardListener::HandlerMap();
-HHOOK KeyboardListener::hook_                               = NULL;
-
-LRESULT CALLBACK KeyboardListener::KeyboardProc(int code, WPARAM wParam, LPARAM lParam)
+namespace controlkit::__deprecated
 {
-    KBDLLHOOKSTRUCT* ks = (KBDLLHOOKSTRUCT*)lParam;
-    for (const auto& it : handler_map_)
+    HHOOK KeyboardListener::hook_                               = NULL;
+    KeyboardListener::Handler KeyboardListener::handler_        = nullptr;
+    KeyboardListener::HandlerMap KeyboardListener::handler_map_ = KeyboardListener::HandlerMap();
+
+    LRESULT CALLBACK KeyboardListener::KeyboardProc(int code, WPARAM wParam, LPARAM lParam)
     {
-        if (!it.second(ks->vkCode, wParam))
+        KBDLLHOOKSTRUCT* ks = (KBDLLHOOKSTRUCT*)lParam;
+        for (const auto& it : handler_map_)
         {
-            return 1;
+            if (!it.second(ks->vkCode, wParam))
+            {
+                return 1;
+            }
         }
+        return CallNextHookEx(NULL, code, wParam, lParam);
     }
-    return CallNextHookEx(NULL, code, wParam, lParam);
+
+    void KeyboardListener::Stop()
+    {
+        if (hook_ != NULL)
+            UnhookWindowsHookEx(hook_);
+        hook_ = NULL;
+    }
+
+    void KeyboardListener::Start() { hook_ = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, NULL, 0); }
+    void KeyboardListener::RegisterHandler(HandlerId id, const Handler& handler) { handler_map_[id] = handler; }
+    void KeyboardListener::RemoveHandler(HandlerId id) { handler_map_.erase(id); }
+
+} // namespace controlkit::__deprecated
+
+/* v2 implementation */
+
+KeyboardListener& KeyboardListener::Instance()
+{
+    static KeyboardListener instance;
+    return instance;
+}
+KeyboardListener::KeyboardListener() : impl_() {}
+KeyboardListener::~KeyboardListener() {}
+
+KeyboardListener::Impl::Impl() : hook_(NULL)
+{
 }
 
-void KeyboardListener::Stop()
+KeyboardListener::Impl::~Impl() { Stop(); }
+void KeyboardListener::Impl::Start() { hook_ = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, NULL, 0); }
+void KeyboardListener::Impl::Stop()
 {
     if (hook_ != NULL)
         UnhookWindowsHookEx(hook_);
     hook_ = NULL;
 }
-
-void KeyboardListener::Start() { hook_ = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, NULL, 0); }
-void KeyboardListener::RegisterHandler(HandlerId id, const Handler& handler) { handler_map_[id] = handler; }
-void KeyboardListener::RemoveHandler(HandlerId id) { handler_map_.erase(id); }
+void KeyboardListener::Impl::RegisterHandler(HandlerId id, const Handler& handler) { handler_map_[id] = handler; }
+void KeyboardListener::Impl::RemoveHandler(HandlerId id) { handler_map_.erase(id); }
+KeyboardListener::Impl::HandlerMap KeyboardListener::Impl::handler_map_ = KeyboardListener::Impl::HandlerMap();
+LRESULT CALLBACK KeyboardListener::Impl::KeyboardProc(int code, WPARAM wParam, LPARAM lParam)
+{
+    KBDLLHOOKSTRUCT* ks = (KBDLLHOOKSTRUCT*)lParam;
+    for (const auto& it : handler_map_)
+    {
+        if (!it.second(ks->vkCode, wParam))
+            return 1;
+    }
+    return CallNextHookEx(NULL, code, wParam, lParam);
+}
--- project/keyboard.h
+++ project/keyboard.h
@@ -10,30 +10,78 @@
 
 namespace controlkit
 {
+
+    namespace __deprecated
+    {
+        class KeyboardListener
+        {
+        public:
+            using KeyCode    = DWORD;
+            using KeyState   = WPARAM;
+            using Handler    = std::function<bool(KeyCode, KeyState)>;
+            using HandlerId  = KeyboardHandlerEnum;
+            using HandlerMap = std::map<HandlerId, Handler>;
+
+            static constexpr KeyState KeyPressed  = 256;
+            static constexpr KeyState KeyReleased = 257;
+
+            static void Start();
+            static void Stop();
+            static LRESULT CALLBACK KeyboardProc(int code, WPARAM wParam, LPARAM lParam);
+            static void ResetHandlerList();
+            static void RegisterHandler(HandlerId id, const Handler& handler);
+            static void RemoveHandler(HandlerId);
+
+        private:
+            static Handler handler_;
+            static HHOOK hook_;
+            static HandlerMap handler_map_;
+        };
+    } // namespace __deprecated
+
     class KeyboardListener
     {
     public:
-        using KeyCode    = DWORD;
-        using KeyState   = WPARAM;
-        using Handler    = std::function<bool(KeyCode, KeyState)>;
-        using HandlerId  = KeyboardHandlerEnum;
-        using HandlerMap = std::map<HandlerId, Handler>;
+        using KeyCode   = DWORD;
+        using KeyState  = WPARAM;
+        using Handler   = std::function<bool(KeyCode, KeyState)>;
+        using HandlerId = KeyboardHandlerEnum;
 
         static constexpr KeyState KeyPressed  = 256;
         static constexpr KeyState KeyReleased = 257;
 
-        static void Start();
-        static void Stop();
-        static LRESULT CALLBACK KeyboardProc(int code, WPARAM wParam, LPARAM lParam);
-        static void ResetHandlerList();
-        static void RegisterHandler(HandlerId id, const Handler& handler);
-        static void RemoveHandler(HandlerId);
+        inline static void Start() { Instance().impl_.Start(); }
+        inline static void Stop() { Instance().impl_.Stop(); }
+        inline static void RegisterHandler(HandlerId id, const Handler& handler) { Instance().impl_.RegisterHandler(id, handler); }
+        inline static void RemoveHandler(HandlerId id) { Instance().impl_.RemoveHandler(id); }
 
-    private:
-        static Handler handler_;
-        static HHOOK hook_;
-        static HandlerMap handler_map_;
+        ~KeyboardListener();
+
+    protected:
+        class Impl
+        {
+        public:
+            using HandlerMap = std::map<HandlerId, Handler>;
+
+            Impl();
+            ~Impl();
+
+            void Start();
+            void Stop();
+            void RegisterHandler(HandlerId id, const Handler& handler);
+            void RemoveHandler(HandlerId);
+
+        protected:
+            HHOOK hook_;
+            static HandlerMap handler_map_;
+            static LRESULT CALLBACK KeyboardProc(int code, WPARAM wParam, LPARAM lParam);
+        };
+
+        Impl impl_;
+        KeyboardListener();
+        static KeyboardListener& Instance();
     };
+
 } // namespace controlkit
 
 #endif // KEYBOARD_H
